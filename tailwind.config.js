8ccbeb4303b54658c6b1734b376fd9acf7820c38
Muhamad Rizky <mrizky@binar.co.id>
2022-07-24 23:17:12 +0700

    finish page1

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,22 @@ module.exports = {
         sans: ['Open Sans', 'sans-serif'],
         neue: 'Bebas Neue',
       },
+      fontSize: {
+        '5.5xl': ['54px', '62px'],
+      },
       backgroundImage: {
-        'background-page1': "url('/dist/assets/main-bg.jpg')",
-        'background-page2': "url('/dist/assets/the-games-bg.jpg')",
+        'background-page1': "radial-gradient(175% 57.77% at 50% 40%, rgba(255, 180, 52, 0) 0%, rgba(55, 44, 33, 0.10) 70%,#08090A 100%),url('/dist/assets/main-bg.jpg')",
+        'background-page2': "radial-gradient(175% 10% at 50% 75%, rgba(255, 180, 52, 0) 0%, rgba(55, 44, 33, 0.10) 70%,#08090A 800%),url('/dist/assets/the-games-bg.jpg')",
         'background-page3': "url('/dist/assets/features.jpg')",
         'background-page4': "url('/dist/assets/requirements.png')",
         'background-page5': "url('/dist/assets/top-score-bg.png')",
       },
       spacing: {
         '2.8': '0.8rem',
+        '120': '34rem',
+      },
+      animation: {
+        'scrolldown': 'scrollDown 1.2s infinite',
       }
     },
   },
